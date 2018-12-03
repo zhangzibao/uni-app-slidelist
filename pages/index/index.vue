9128676bcbe23be2122e2679da0d89c1c6e8e78a
张自爆 <1078763967@qq.com>
2018-12-03 18:01:48 +0800

    Initial commit

---
+++ pages/index/index.vue
@@ -0,0 +1,289 @@
+<template>
+    <div class="content">
+        <div class="container_of_slide" v-for="(item,index) in commitInfo" :key="index">
+            <div class="slide_list"
+                 @touchstart="touchStart($event,index)"
+                 @touchend="touchEnd($event,index)"
+                 @touchmove="touchMove($event,index)"
+                 :style="{transform:'translate3d('+item.slide_x+'px, 0, 0)'}"
+            >
+                <div class="now-message-info"
+                     :style="{width:Screen_width+'px'}"
+                >
+                    <div class="imgInfo" @click="recover(index)">
+                        <img :src="item.img">
+                    </div>
+                    <div class="centerInfo">
+                        <p class="name">
+                            {{item.name}}
+                        </p>
+                        <p class="message">
+                            {{item.message}}
+                        </p>
+                    </div>
+                    <div class="timeInfo" @click="recover(index)">
+                        <div class="time">
+                            <text>{{item.time}}</text>
+                        </div>
+                    </div>
+                </div>
+                <div class="group-btn">
+                    <div class="top" @click="top(index)" v-if="item.top">
+                        取消置顶
+                    </div>
+                    <div class="top" @click="top(index)" v-else>
+                        置顶
+                    </div>
+                    <div class="delect" @click="delect(index)">
+                        删除
+                    </div>
+                </div>
+                <div style="clear:both"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'slide-list',
+    computed: {
+        Screen_width() {
+            return uni.getSystemInfoSync().windowWidth;
+        }
+    },
+    data() {
+        return {
+            startX: 0,
+            LastX: 0,
+            btnWidth: 0,
+            commitInfo: [
+                {
+                    img: '../../static/img/1.jpg',
+                    name: '狐狸叔叔',
+                    message: '今晚去吃饭吗?',
+                    time: '19:08',
+                    top: 0,
+                    slide_x: 0
+                },
+
+                {
+                    img: '../../static/img/2.jpg',
+
+                    name: '老虎爸爸',
+                    message:'黑发不知勤学早，白首方悔读书迟。 —— 颜真卿《劝学诗》',
+                    time: '02:08',
+                    top: 0,
+                    slide_x: 0
+                },
+                {
+                    img: '../../static/img/3.jpg',
+                    name: '偶遇妹子',
+                    message: '忽如一夜春风来，千树万树梨花开。 —— 岑参《白雪歌送武判官归京》',
+                    time: '02:08',
+                    top: 0,
+                    slide_x: 0
+                },
+                {
+                    img: '../../static/img/4.jpg',
+                    name: '男神',
+                    message: '寂寞空庭春欲晚，梨花满地不开门。 —— 刘方平《春怨》',
+                    time: '23:08',
+                    top: 0,
+                    slide_x: 0
+                },
+                {
+                   img: '../../static/img/5.jpg',
+                    name: '妹妹',
+                    message: '人生若只如初见，何事秋风悲画扇。 —— 纳兰性德《木兰词·拟古决绝词柬友》',
+                    time: '02:08',
+                    top: 0,
+                    slide_x: 0
+                }
+            ]
+        };
+    },
+    methods: {
+        // 滑动开始
+        touchStart(e, index) {
+            // 获取当前按钮组宽度
+            uni.createSelectorQuery()
+                .selectAll('.group-btn')
+                .boundingClientRect()
+                .exec(res => {
+                    if (res[0] != null) {
+                        this.btnWidth = res[0][index].width * -1;
+                    }
+                });
+            // 记录上一次开始时手指所处位置
+            this.startX = e.mp.changedTouches[0].clientX;
+            // 记录上一次手指位置
+            this.LastX = this.startX;
+            //初始化非当前滑动消息列的位置
+            this.commitInfo.forEach((item, eq) => {
+                if (eq !== index) {
+                    item.slide_x = 0;
+                }
+            });
+        },
+        // 滑动中
+        touchMove(e, index) {
+            const endX = e.mp.changedTouches[0].clientX;
+            const distance = endX - this.LastX;
+            // 预测滑块所处位置
+            const duang = this.commitInfo[index].slide_x + distance;
+            // 如果在可行区域内
+            if (duang <= 0 && duang >= this.btnWidth) {
+                this.commitInfo[index].slide_x += distance;
+            }
+            // 记录上一次开始时手指所处位置
+            this.LastX = endX;
+        },
+        // 滑动结束
+        touchEnd(e, index) {
+            const endX = e.mp.changedTouches[0].clientX;
+            // 判断手指最终位置与手指开始位置的位置差距
+            if (this.startX - endX > 10) {
+                this.commitInfo[index].slide_x = this.btnWidth;
+            } else if (this.startX - endX < -10) {
+                this.commitInfo[index].slide_x = 0;
+            }
+        },
+        // 点击回复原状
+        recover(index) {
+            this.commitInfo[index].slide_x = 0;
+            this.commitInfo.sort((a, b) => {
+                return b.top - a.top;
+            });
+        },
+        // 置顶
+        top(index) {
+            this.commitInfo[index].top = this.commitInfo[index].top ? 0 : 1;
+            this.recover(index);
+        },
+        // 删除
+        delect(index) {
+            this.commitInfo.splice(index, 1);
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+.container_of_slide {
+    width: 100%;
+    overflow: hidden;
+
+    .slide_list {
+        transition: all 100ms;
+        transition-timing-function: ease-out;
+        min-width: 200%;
+        height: 150rpx;
+        border-bottom: 1px solid #e0eef1;
+
+        .now-message-info {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            .imgInfo {
+                width: 100rpx;
+                height: 100rpx;
+                border-radius: 50%;
+                background-color: #38a7fa;
+                margin-left: 4%;
+
+                img {
+                    width: 100rpx;
+                    height: 100rpx;
+                    border-radius: 50%;
+                    overflow: hidden;
+                }
+            }
+
+            .centerInfo {
+                height: 150rpx;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: baseline;
+                text-align: left;
+				width: 60%;
+                p {
+                    overflow: hidden;
+                    white-space: nowrap;
+                    text-overflow: ellipsis;
+					max-width: 100%;
+                }
+                .name {
+                    font-size: 35rpx;
+                }
+
+                .message {
+                    font-size: 24rpx;
+                    color: #7c8489;
+                }
+            }
+
+            .timeInfo {
+                height: 150rpx;
+                display: flex;
+                /*min-width:15% ;*/
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                margin-right: 2%;
+
+                .time {
+                    color: #92a0a1;
+                    font-size: 25rpx;
+                }
+
+                .infoNum {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    height: 30rpx;
+                    border-radius: 10rpx;
+                    background-color: #93d5ed;
+                }
+            }
+        }
+
+        .now-message-info,
+        .group-btn {
+            float: left;
+        }
+
+        .group-btn {
+            display: flex;
+            flex-direction: row;
+            height: 100%;
+            min-width: 100rpx;
+            align-items: center;
+
+            div {
+                height: 100%;
+                color: #fff;
+                text-align: center;
+                padding: 0 50rpx;
+                font-size: 34rpx;
+                line-height: 150rpx;
+            }
+
+            .top {
+                background-color: #c4c7cd;
+            }
+
+            .delect {
+                background-color: #ff3b32;
+            }
+        }
+    }
+}
+</style>
